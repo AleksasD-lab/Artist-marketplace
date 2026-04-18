d602832e39ae70b72c5d44024ccfc120b6a1f3a0
AleksasD <aleksoelpastas@gmail.com>
2026-04-18 11:24:52 +0300

    Create project

---
+++ src/main/java/io/github/adaujotas/artist_marketplace/ArtistMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package io.github.adaujotas.artist_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/adaujotas/artist_marketplace/ArtistMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adaujotas.artist_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
